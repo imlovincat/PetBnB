e1fa70447f4c47726e8fbed786723a4e905578e6
imlovincat <imlovincat@gmail.com>
2021-04-26 09:58:12 +0100

    Pet sitter sign up updated

--- data_utils.py
+++ data_utils.py
@@ -9,15 +9,26 @@ connection = {
 
 
 def customer_signup_data(data):
-
-    with DBcm.UseDatabase(connection) as cursor:
-        SQL = """ INSERT INTO customer (email,password) VALUES (%s, %s) """
-        cursor.execute(SQL, (data["email"], data["password"]),)
+    try: 
+        with DBcm.UseDatabase(connection) as cursor:
+            SQL = """ INSERT INTO customer (email,password) VALUES (%s, %s) """
+            cursor.execute(SQL, (data["email"], data["password"]),)
+    except:
+        print("Error")
 
 
 def customer_login_data(data):
+    try:
+        with DBcm.UseDatabase(connection) as cursor:
+            SQL = """SELECT id, email FROM customer WHERE email = %s AND password = %s"""
+            cursor.execute(SQL, (data["email"], data["password"]),)
+            data = cursor.fetchall()
+            return data
+    except:
+        print("Error")
+
+def petsitter_signup_data(data):
+
     with DBcm.UseDatabase(connection) as cursor:
-        SQL = """SELECT id, email FROM customer WHERE email = %s AND password = %s"""
+        SQL = """ INSERT INTO petsitter (email,password) VALUES (%s, %s) """
         cursor.execute(SQL, (data["email"], data["password"]),)
-        data = cursor.fetchall()
-    return data
